69d954acb91ee49f348582627be4b749ad1ad9e8
= <agustinmida530@gmail.com>
2021-11-13 11:08:55 +0700

    initialfirst

--- kubus.java
+++ kubus.java
@@ -1,28 +1,44 @@
 
+   
 import java.util.Scanner;
 
 public class kubus {
+	public static void main(String[] args) {
 
-    static Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
+		int sisi, tinggi, alas, nim;
+		float luas;
+		float volumekubus, volumelimas,volumetotal;
+		String nama,kelas;
 
-    public static void main(String[] args) {
+		System.out.println("Masukkan Nama\t\t: ");
+		nama = sc.nextLine();
+		System.out.println("Masukkan Kelas\t\t: ");
+		kelas = sc.nextLine();
+		System.out.println("Masukkan Nim\t\t: ");
+		nim = sc.nextInt();
 
-        int sisi , volume;
+		System.out.println("Masukan sisi kubus\t: ");
+		sisi = sc.nextInt();
+		System.out.println("Masukkan tinggi limas\t\t: ");
+		tinggi = sc.nextInt();
 
-        System.out.println("program perhitungan volume kubus");
 
-        System.out.println("masukkan sisi kubus”);
+		luas = sisi * sisi;
+		volumekubus = sisi * sisi * sisi;
+		volumelimas = ((1F/3F) * luas * tinggi);
+		volumetotal = volumekubus + volumelimas;
 
-        sisi = sc.nextInt();
-
-               
-
-        volume = (sisi*sisi*sisi);
-
-        System.out.println("volume kubus adalah "+volume);
-
-    }
-
+		System.out.println("Nama Saya  " + nama);
+		System.out.println("Kelas Saya  " + kelas);
+		System.out.println("Nim Saya " + nim);
+		System.out.println(String.format("Luas salah satu sisi kubus adalah %.2f", luas));
+		System.out.println(String.format("Volume Kubus %.2f", volumekubus));
+		System.out.println(String.format("Volume Limas %.2f", volumelimas));
+		System.out.println(String.format("Volume Total %.2f", volumetotal));
+		
+	}
 }
+
     
 
