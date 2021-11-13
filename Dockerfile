0007382fbb3522ece31abb1e8e548792c24ad7d1
= <agustinmida530@gmail.com>
2021-11-13 10:50:59 +0700

    initialfirst

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,10 @@ MAINTAINER agustindwinurhamidah <1931710076@student.polinema.ac.id>
 RUN mkdir -p /java
 WORKDIR /java
 
-COPY JajarGenjang.java /java
+COPY kubus.java /java
 
 #compile file java
-RUN javac JajarGenjang.java
+RUN javac kubus.java
 
 #running java
-ENTRYPOINT java JajarGenjang
+ENTRYPOINT java kubus
---
+++ kubus.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class kubus {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int sisi , volume;
+
+        System.out.println(“program perhitungan volume kubus”);
+
+        System.out.println(“masukkan sisi kubus”);
+
+        sisi = sc.nextInt();
+
+               
+
+        volume = (sisi*sisi*sisi);
+
+        System.out.println(“volume kubus adalah “+volume);
+
+    }
+
+}
+    
+
